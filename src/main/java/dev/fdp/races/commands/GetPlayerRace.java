9e6d9b94512ff21082001e78e822fba05c5ebe55
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-04-06 23:24:43 +0300

    Улучшена валидация входных аргумментов в get_player_race

--- src/main/java/dev/fdp/races/commands/GetPlayerRace.java
+++ src/main/java/dev/fdp/races/commands/GetPlayerRace.java
@@ -3,13 +3,26 @@ package dev.fdp.races.commands;
 import dev.fdp.races.FDP_Races;
 import dev.fdp.races.utils.ChatUtil;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Map;
 
 public class GetPlayerRace extends AbstractSubCommand {
     public void onCommandExecute(CommandSender sender, String[] args) {
-        String playerName = args[1];
+        if (args.length < 2 && !(sender instanceof Player)) {
+            ChatUtil.message(sender, "<red>Вы не указали игрока для получения расы");
+            return;
+        }
+
+        String playerName = args.length < 2 ? sender.getName() : args[1];
+
         String playerRace = FDP_Races.getInstance().raceManager.getPlayerRace(playerName);
-        ChatUtil.message(sender, "<green>У игрока <yellow>{player} <green>расса <yellow>{race}", Map.of("{player}", playerName, "{race}", playerRace));
+
+        if (sender.getName().equals(playerName))
+            ChatUtil.message(sender, "<green>Ваша раса <yellow>{race}",
+                    Map.of("{race}", playerRace));
+        else
+            ChatUtil.message(sender, "<green>У игрока <yellow>{player} <green>раса <yellow>{race}",
+                    Map.of("{player}", playerName, "{race}", playerRace));
     }
 }

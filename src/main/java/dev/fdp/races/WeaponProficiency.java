7fc55484ccba4b73295d9e7f093515396b32cded
mikinol_termux <sm44aksdmiki13877kfh@gmail.com>
2025-04-24 10:54:11 +0300

    Добавлен lombok

--- src/main/java/dev/fdp/races/WeaponProficiency.java
+++ src/main/java/dev/fdp/races/WeaponProficiency.java
@@ -1,5 +1,8 @@
 package dev.fdp.races;
 
+import lombok.Data;
+
+@Data
 public class WeaponProficiency {
   private double bowDamageMultiplier = 1.0;
   private double bowProjectileSpeedMultiplier = 1.0;
@@ -11,85 +14,4 @@ public class WeaponProficiency {
   private double damageAdditionalWithIronAndLowerArmor = 0.0;
   private double damageAdditionalWithWolfsNear = 0.0;
   private double dualWeaponDamageAdditional = 0.0;
-
-  // Геттеры и сеттеры
-  public double getBowDamageMultiplier() {
-    return bowDamageMultiplier;
-  }
-
-  public void setBowDamageMultiplier(double bowDamageMultiplier) {
-    this.bowDamageMultiplier = bowDamageMultiplier;
-  }
-
-  public double getDamageAdditionalWithIronAndLowerArmor() {
-    return damageAdditionalWithIronAndLowerArmor;
-  }
-
-  public void setDamageAdditionalWithIronAndLowerArmor(double damageAdditionalWithIronAndLowerArmor) {
-    this.damageAdditionalWithIronAndLowerArmor = damageAdditionalWithIronAndLowerArmor;
-  }
-
-  public double getDamageAdditionalWithWolfsNear() {
-    return damageAdditionalWithWolfsNear;
-  }
-
-  public void setDamageAdditionalWithWolfsNear(double damageAdditionalWithWolfsNear) {
-    this.damageAdditionalWithWolfsNear = damageAdditionalWithWolfsNear;
-  }
-
-  public double getBowProjectileSpeedMultiplier() {
-    return bowProjectileSpeedMultiplier;
-  }
-
-  public void setBowProjectileSpeedMultiplier(double bowProjectileSpeedMultiplier) {
-    this.bowProjectileSpeedMultiplier = bowProjectileSpeedMultiplier;
-  }
-
-  public double getSwordDamageMultiplier() {
-    return swordDamageMultiplier;
-  }
-
-  public void setSwordDamageMultiplier(double swordDamageMultiplier) {
-    this.swordDamageMultiplier = swordDamageMultiplier;
-  }
-
-  public double getAxeDamageMultiplier() {
-    return axeDamageMultiplier;
-  }
-
-  public void setAxeDamageMultiplier(double axeDamageMultiplier) {
-    this.axeDamageMultiplier = axeDamageMultiplier;
-  }
-
-  public double getMaceDamageMultiplier() {
-    return maceDamageMultiplier;
-  }
-
-  public void setMaceDamageMultiplier(double maceDamageMultiplier) {
-    this.maceDamageMultiplier = maceDamageMultiplier;
-  }
-
-  public double getHandDamageAdditional() {
-    return handDamageAdditional;
-  }
-
-  public void setHandDamageAdditional(double handDamageAdditional) {
-    this.handDamageAdditional = handDamageAdditional;
-  }
-
-  public double getDamageAdditional() {
-    return damageAdditional;
-  }
-
-  public void setDamageAdditional(double damageAdditional) {
-    this.damageAdditional = damageAdditional;
-  }
-
-  public double getDualWeaponDamageAdditional() {
-    return dualWeaponDamageAdditional;
-  }
-
-  public void setDualWeaponDamageAdditional(double dualWeaponDamageAdditional) {
-    this.dualWeaponDamageAdditional = dualWeaponDamageAdditional;
-  }
 }

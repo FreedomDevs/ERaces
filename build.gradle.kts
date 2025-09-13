09390e58dc1e23af79886ed187792947e2fada6c
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-09-13 14:35:17 +0300

    Добавлен контлин

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     java
     `maven-publish`
-   // kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "2.2.0-RC2"
+    kotlin("plugin.serialization") version "2.2.0-RC2"
+    id("com.gradleup.shadow") version "8.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "dev.elysium.eraces"
@@ -18,15 +21,41 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib"))
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     compileOnly("org.projectlombok:lombok:1.18.30")
-    //implementation(kotlin("stdlib"))
-    //testImplementation(kotlin("test"))
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
-java { toolchain.languageVersion.set(JavaLanguageVersion.of(21)) }
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+}
 
-tasks.processResources { filesMatching("plugin.yml") { expand(mapOf("version" to version)) } }
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.1")
+    }
+
+    // jar = shadowJar
+    build {
+        dependsOn("shadowJar")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
 
 publishing {
     publications {

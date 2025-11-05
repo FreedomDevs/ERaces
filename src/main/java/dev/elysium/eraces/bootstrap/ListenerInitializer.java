5d05151c637d40760981d119e3be39ee9eef0bac
Foksif <knenrerp@gmail.com>
2025-11-05 17:54:01 +0700

    Листенеры и логер

---
+++ src/main/java/dev/elysium/eraces/bootstrap/ListenerInitializer.java
@@ -0,0 +1,23 @@
+package dev.elysium.eraces.bootstrap;
+
+import dev.elysium.eraces.ERaces;
+import dev.elysium.eraces.exceptions.InitFailedException;
+import dev.elysium.eraces.listeners.GuiListener;
+import dev.elysium.eraces.listeners.RaceSelectListener;
+import dev.elysium.eraces.listeners.PluginMessageListener;
+import org.bukkit.Bukkit;
+
+public class ListenerInitializer implements IInitializer {
+    @Override
+    public void setup(ERaces plugin) {
+        try {
+            Bukkit.getPluginManager().registerEvents(RaceSelectListener.INSTANCE, plugin);
+            plugin.getServer().getMessenger().registerIncomingPluginChannel(plugin, "elysium:eraces_cast", new PluginMessageListener());
+
+            GuiListener.INSTANCE.init(plugin);
+            Bukkit.getPluginManager().registerEvents(GuiListener.INSTANCE, plugin);
+        } catch (Exception e) {
+            throw new InitFailedException("Ошибка при регистрации слушателей", e);
+        }
+    }
+}

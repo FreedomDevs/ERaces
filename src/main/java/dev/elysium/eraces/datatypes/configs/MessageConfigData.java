b9b934a3888426c4108a168595c37bc869f103b1
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-11-18 18:12:48 +0300

    Удалил ненужные message-ы

--- src/main/java/dev/elysium/eraces/datatypes/configs/MessageConfigData.java
+++ src/main/java/dev/elysium/eraces/datatypes/configs/MessageConfigData.java
@@ -12,38 +12,17 @@ public class MessageConfigData {
     @ConfigsProperty(path = "plugin.disabled", defaultString = "Плагин выключен")
     public String pluginDisabled;
 
-    @ConfigsProperty(path = "commands.get_player_race.race_check_error", defaultString = "<red>Вы не указали игрока для получения расы")
-    public String getPlayerRaceError;
-
     @ConfigsProperty(path = "commands.get_player_race.race_check_success", defaultString = "<green>У игрока <yellow>{player} <green>раса <yellow>{race}")
     public String getPlayerRaceSuccess;
 
     @ConfigsProperty(path = "commands.get_player_race.race_check_success_me", defaultString = "<green>Ваша раса <yellow>{race}")
     public String getPlayerRaceSuccessMe;
 
-    @ConfigsProperty(path = "commands.races.subcommand_error", defaultString = "<red>Не указана подкоманда")
-    public String racesSubcommandError;
-
-    @ConfigsProperty(path = "commands.races.subcommand_is_null", defaultString = "<red>Подкоманда: <yellow>{arg} <red>не существует")
-    public String racesSubcommandIsNull;
-
-    @ConfigsProperty(path = "commands.regenerate_player_race.console_usage", defaultString = "<red>Использование из консоли: /command <ник игрока>")
-    public String regenerateConsoleUsage;
-
-    @ConfigsProperty(path = "commands.regenerate_player_race.nick_error", defaultString = "<red>Неверно указан ник игрока")
-    public String regenerateNickError;
-
     @ConfigsProperty(path = "commands.regenerate_player_race.regenerate_success", defaultString = "<green>Раса игрока <yellow>{player} <green>установлена на: <yellow>{race}")
     public String regenerateSuccess;
 
-    @ConfigsProperty(path = "commands.set_player_race.small_args", defaultString = "<red>Мало аргументов")
-    public String setPlayerRaceSmallArgs;
-
     @ConfigsProperty(path = "commands.set_player_race.race_not_found", defaultString = "<red>Такая раса не существует")
     public String setPlayerRaceNotFound;
-
-    @ConfigsProperty(path = "commands.set_player_race.nick_null", defaultString = "<red>Неверно указан ник игрока")
-    public String setPlayerRaceNickNull;
 
     @ConfigsProperty(path = "commands.set_player_race.set_success", defaultString = "<green>Раса игрока <yellow>{player} <green>установлена на: <yellow>{race}")
     public String setPlayerRaceSuccess;

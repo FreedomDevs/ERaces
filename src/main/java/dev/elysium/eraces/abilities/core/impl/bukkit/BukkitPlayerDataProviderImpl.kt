06be247f8301a164e70c86fa3ccdcc80269caa5a
Foksif <knenrerp@gmail.com>
2025-11-23 12:44:53 +0700

    чистка PlayerData

--- src/main/java/dev/elysium/eraces/abilities/core/impl/bukkit/BukkitPlayerDataProviderImpl.kt
+++ src/main/java/dev/elysium/eraces/abilities/core/impl/bukkit/BukkitPlayerDataProviderImpl.kt
@@ -6,13 +6,11 @@ import dev.elysium.eraces.abilities.core.registry.PlayerRaceData
 import org.bukkit.entity.Player
 
 class BukkitPlayerDataProviderImpl : IPlayerDataProvider {
-    private val plugin: ERaces = ERaces.getInstance()
-
-    override fun getPlayerRace(player: Player): PlayerRaceData? {
-        val race = plugin.context.playerDataManager.getPlayerRace(player) ?: return null
-        return PlayerRaceData(
-            raceId = race.id.toString(),
-            abilities = race.abilities.toSet()
-        )
-    }
+    override fun getPlayerRace(player: Player): PlayerRaceData? =
+        ERaces.getInstance().context.playerDataManager.getPlayerRace(player)?.let { race ->
+            PlayerRaceData(
+                raceId = race.id.toString(),
+                abilities = race.abilities.toSet()
+            )
+        }
 }

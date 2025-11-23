6fd2d75ca9671bd0f34430a76f5d0874fa9bc10e
Foksif <knenrerp@gmail.com>
2025-11-23 12:43:34 +0700

    чистка маны

--- src/main/java/dev/elysium/eraces/abilities/core/impl/bukkit/BukkitManaProviderImpl.kt
+++ src/main/java/dev/elysium/eraces/abilities/core/impl/bukkit/BukkitManaProviderImpl.kt
@@ -7,9 +7,8 @@ import org.bukkit.entity.Player
 class BukkitManaProviderImpl : IManaProvider {
     private val plugin: ERaces = ERaces.getInstance()
 
-    override fun getMana(player: Player): Double {
-        return plugin.context.manaManager.getMana(player)
-    }
+    override fun getMana(player: Player): Double =
+        plugin.context.manaManager.getMana(player)
 
     override fun useMana(player: Player, amount: Double) {
         plugin.context.manaManager.useMana(player, amount)

f6f006021409583ac5117de829da3f6fe78e2301
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-11-30 16:00:14 +0300

    Revert "сделана способность: магический радиус"
    This reverts commit d448a30e72cb8a8ef5b350d1f71f7070a30c487d.

--- src/main/java/dev/elysium/eraces/abilities/abils/buffs/mana/TheMagicRadiusAbility.kt
+++ src/main/java/dev/elysium/eraces/abilities/abils/buffs/mana/TheMagicRadiusAbility.kt
@@ -2,6 +2,8 @@ package dev.elysium.eraces.abilities.abils.buffs.mana
 
 import dev.elysium.eraces.ERaces
 import dev.elysium.eraces.abilities.ConfigHelper
+import dev.elysium.eraces.abilities.ConfigHelper.read
+import dev.elysium.eraces.abilities.ConfigHelper.write
 import dev.elysium.eraces.abilities.RegisterAbility
 import dev.elysium.eraces.abilities.abils.base.BaseCooldownAbility
 import dev.elysium.eraces.utils.targetUtils.Target

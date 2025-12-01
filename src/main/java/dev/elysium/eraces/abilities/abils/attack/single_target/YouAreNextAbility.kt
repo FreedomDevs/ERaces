0e9b74e9959e811311256db1f4a283a7e39253ff
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-12-01 17:46:23 +0300

    Способность вроде как доделана

--- src/main/java/dev/elysium/eraces/abilities/abils/attack/single_target/YouAreNextAbility.kt
+++ src/main/java/dev/elysium/eraces/abilities/abils/attack/single_target/YouAreNextAbility.kt
@@ -1,8 +1,13 @@
 package dev.elysium.eraces.abilities.abils.attack.single_target
 
 import dev.elysium.eraces.abilities.ConfigHelper
+import dev.elysium.eraces.abilities.ConfigHelper.read
+import dev.elysium.eraces.abilities.ConfigHelper.write
 import dev.elysium.eraces.abilities.RegisterAbility
 import dev.elysium.eraces.abilities.abils.base.BaseEffectsAbility
+import dev.elysium.eraces.utils.msg
+import dev.elysium.eraces.utils.targetUtils.Target
+import dev.elysium.eraces.utils.targetUtils.target.TargetFilter
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.potion.PotionEffectType
@@ -20,7 +25,30 @@ class YouAreNextAbility : BaseEffectsAbility(
     private var radius = 40.0
 
     override fun customActivate(player: Player) {
+        var isExecuted = false
 
+        Target.from(player)
+            .filter(TargetFilter.ENTITIES, TargetFilter.FIRST_ENTITY)
+            .inEye(radius, 0.2)
+            .execute { target ->
+                run {
+                    val loc = target.location.clone()
+                    val direction = target.eyeLocation.direction.normalize()
+                    loc.add(direction.multiply(-1.5))
+                    loc.y = target.location.y
+
+                    player.teleport(loc)
+
+                    player.location.world.strikeLightningEffect(player.location)
+                    player.damage(selfDamage, player)
+                    isExecuted = true
+                }
+            }
+
+        if (!isExecuted) {
+            player.msg("<red>Вы не смотрите на игрока, способность не активирована!")
+            this.resetCooldown(player)
+        }
     }
 
     override fun loadCustomParams(cfg: YamlConfiguration) {

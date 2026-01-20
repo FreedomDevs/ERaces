33179c599226ed848f2158aae35f900e0cf7ce69
Foksif <knenrerp@gmail.com>
2026-01-20 17:23:55 +0700

    onInteract готов

--- src/main/java/dev/elysium/eraces/items/weapon/Weapon.kt
+++ src/main/java/dev/elysium/eraces/items/weapon/Weapon.kt
@@ -5,6 +5,8 @@ import dev.elysium.eraces.items.core.Item
 import dev.elysium.eraces.items.core.ItemType
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.inventory.EquipmentSlot
 
 abstract class Weapon: Item {
     final override val type = ItemType.WEAPON
@@ -12,4 +14,8 @@ abstract class Weapon: Item {
     open fun onHit(event: EntityDamageByEntityEvent)  {}
 
     open fun onInventory(playersWithSlots: Map<Player, Set<SlotType>>) {}
+
+    open fun onInteractOffHand(player: Player) {}
+
+    open fun onInteract(player: Player, hand: EquipmentSlot, click: ClickType) {}
 }
--- src/main/java/dev/elysium/eraces/items/weapon/WeaponListener.kt
+++ src/main/java/dev/elysium/eraces/items/weapon/WeaponListener.kt
@@ -1,18 +1,86 @@
 package dev.elysium.eraces.items.weapon
 
+import org.bukkit.event.block.Action
 import dev.elysium.eraces.items.core.ItemResolver
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
 
-class WeaponListener: Listener {
+class WeaponListener : Listener {
     @EventHandler
     fun onHit(e: EntityDamageByEntityEvent) {
         val player = e.damager as? Player ?: return
-        val weapon = ItemResolver.resolve(player.inventory.itemInMainHand)
-                as? Weapon ?: return
+        val (weapon, _) = getWeaponToMainOrOffHand(player, Hand.MAIN_HAND) ?: return
 
         weapon.onHit(e)
+    }
+
+    @EventHandler
+    fun onInteract(e: PlayerInteractEvent) {
+        val clickType = actionToOnClickType(e.action) ?: return
+        val slot = e.hand ?: return
+
+        val hand = when (slot) {
+            EquipmentSlot.HAND -> Hand.MAIN_HAND
+            EquipmentSlot.OFF_HAND -> Hand.OFF_HAND
+            else -> return
+        }
+
+        val (weapon, _) = getWeaponToMainOrOffHand(e.player, hand) ?: return
+
+        weapon.onInteract(e.player, slot, clickType)
+    }
+
+    /*
+    * HELPERS
+    */
+
+    private fun actionToOnClickType(action: Action): ClickType? {
+        val clickType: ClickType? = when (action) {
+            Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> ClickType.LEFT
+            Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> ClickType.RIGHT
+            else -> null
+        }
+
+        return clickType
+    }
+
+    private fun getWeaponToMainOrOffHand(player: Player, hand: Hand = Hand.ALL): Pair<Weapon, Hand>? {
+        var weapon: Weapon
+        val inv = player.inventory
+
+        fun main(): Pair<Weapon, Hand>? {
+            if (ItemResolver.resolve(inv.itemInMainHand) is Weapon) {
+                weapon = ItemResolver.resolve(inv.itemInMainHand) as? Weapon ?: return null
+                return Pair(weapon, Hand.MAIN_HAND)
+            }
+            return null
+        }
+
+        fun off(): Pair<Weapon, Hand>? {
+            if (ItemResolver.resolve(inv.itemInOffHand) is Weapon) {
+                weapon = ItemResolver.resolve(inv.itemInOffHand) as? Weapon ?: return null
+                return Pair(weapon, Hand.OFF_HAND)
+            }
+            return null
+        }
+
+        when (hand) {
+            Hand.ALL -> main() ?: off()
+            Hand.MAIN_HAND -> main()
+            Hand.OFF_HAND -> off()
+        }
+
+        return null
+    }
+
+    private enum class Hand {
+        MAIN_HAND,
+        OFF_HAND,
+        ALL
     }
 }
